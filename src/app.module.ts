584143ff15e392ef0bf5c4272a72528146d72d38
Rattanachote <52287763+rattanachote@users.noreply.github.com>
2025-10-08 18:19:11 +0700

    Add files via upload
    google sign in

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }), // << สำคัญ
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ถ้าเคยมี global prefix ให้รู้ว่า route จะย้ายเป็น /api/auth/login
+  // app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log('API on http://localhost:3000');
+  app.use(cookieParser());
+  app.enableCors({ origin: 'http://localhost:5173', credentials: true });
 }
 bootstrap();
