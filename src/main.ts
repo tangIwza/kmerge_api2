bb323da9cf62c857aa18859b537a3fbd0dabcc03
“Kritjomza” <kritjomza23@gmail.com>
2025-11-04 21:57:18 +0700

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,10 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'https://k-merge.vercel.app',
+      /\.vercel\.app$/
+    ],
     credentials: true,
   });
 
